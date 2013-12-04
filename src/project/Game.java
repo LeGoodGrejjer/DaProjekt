53725019d1261e45a796d5490f9119645032cc22
nilsnordin <nils.nordin@me.com>
2013-12-04 11:32:43 +0100

    test

--- src/project/Game.java
+++ src/project/Game.java
@@ -18,6 +18,7 @@ public class Game extends JFrame{
 	        setPreferredSize(new Dimension(600, 600));
 	        pack();
 	        //setSize(600, 600);
+	        //jhsfs
 	        setVisible(true);
 	    }
 
