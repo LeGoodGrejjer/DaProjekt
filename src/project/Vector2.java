eeb54a6648fdac7ba3696f40172be7c7bb3493c8
zorroabbe <r@rm.se>
2013-12-04 09:20:37 +0100

    kom i fucking byx

--- src/project/Vector2.java
+++ src/project/Vector2.java
@@ -36,4 +36,31 @@ public class Vector2
 	{
 		return new Vector2(v1.x + v2.x, v1.y + v2.y);
 	}
+	public static Vector2 multiply(Vector2 v1, Vector2 v2)
+	{
+		return new Vector2(v1.x * v2.x, v1.y * v2.y);
+	}
+	public static Vector2 multiply(Vector2 v1, float v2)
+	{
+		return new Vector2(v1.x * v2, v1.y * v2);
+	}
+	public static Vector2 project(Vector2 a, Vector2 b)
+	{
+		float dp = Vector2.dot(a, b);
+		float x = (dp / (b.x * b.x + b.y * b.y)) * b.x;
+		float y = (dp / (b.x * b.x + b.y * b.y)) * b.y;
+		return new Vector2(x, y);
+	}
+	public Vector2 normalize()
+	{
+		float x = this.x, y = this.y;
+		float len_v = (float)Math.sqrt(x * x + y * y);
+		x /= len_v;
+		y /= len_v;
+		return new Vector2(x, y);
+	}
+	public static Vector2 delta(Vector2 v1, Vector2 v2)
+	{
+		return new Vector2(v1.x - v2.x, v1.y - v2.y);
+	}
 }
