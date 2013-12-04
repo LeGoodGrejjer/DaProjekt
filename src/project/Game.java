2a4dd7f8c470c35637b2dec2cc0f20d307fa6dac
Nils Nordin <nils.nordin@me.com>
2013-12-04 10:38:28 +0100

    Cool stuff

--- src/project/Game.java
+++ src/project/Game.java
@@ -20,7 +20,7 @@ public class Game extends JFrame{
 	        //setSize(600, 600);
 	        setVisible(true);
 	    }
-
+	 // hahahahahahahaha
 	    public static void main(String[] args) {
 	        new Game();
 	    }
