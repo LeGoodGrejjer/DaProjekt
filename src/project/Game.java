d1dda472afef6c03b6f23bdda9231c718bdfb7da
nilsnordin <nils.nordin@me.com>
2013-12-04 22:36:01 +0100

    inget vidare

--- src/project/Game.java
+++ src/project/Game.java
@@ -1,10 +1,17 @@
 package project;
 
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
-public class Game extends JFrame{
+public class Game extends JFrame implements MouseListener, MouseMotionListener{
+	
+	private ArrayList<Vector2> pointsArr = new ArrayList<Vector2>();
+	
 	 public Game() {
 		
 		 Level level = new Level();
@@ -19,6 +26,8 @@ public class Game extends JFrame{
 	        setPreferredSize(new Dimension(600, 600));
 	        pack();
 	        
+	        
+	        level.addMouseMotionListener(new MouseDraw());
 	        level.addMouseListener(new MouseDraw());
 	      
 	        
@@ -31,4 +40,53 @@ public class Game extends JFrame{
 	    public static void main(String[] args) {
 	        new Game();
 	    }
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+			pointsArr.add(new Vector2(e.getX(), e.getY()));
+			
+			
+			PhysicsFixture fixture = new CircleFixture(25f);
+	    	physObj = new PhysicsObject(fixture, new Vector2(100f, 300f));
+	    	physObj.name = "Player";
+	    	
+	    	world.addObject(physObj);
+		}
+		
+
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
 }
--- src/project/MouseDraw.java
+++ src/project/MouseDraw.java
@@ -1,17 +1,46 @@
 package project;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Label;
+import java.awt.Rectangle;
+import java.awt.Shape;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.geom.Line2D;
+import java.awt.image.ImageObserver;
+import java.text.AttributedCharacterIterator;
+import java.util.ArrayList;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class MouseDraw extends JPanel implements MouseListener, MouseMotionListener {
 
+	private ArrayList<Vector2> pointsArr = new ArrayList<Vector2>();
+	
+	PhysicsObject physObj;
+	
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
-		System.out.println("drag");
+		pointsArr.add(new Vector2(e.getX(), e.getY()));
+		
+		
+		PhysicsFixture fixture = new CircleFixture(25f);
+    	physObj = new PhysicsObject(fixture, new Vector2(100f, 300f));
+    	physObj.name = "Player";
+    	
+    	world.addObject(physObj);
 	}
 
 	@Override
